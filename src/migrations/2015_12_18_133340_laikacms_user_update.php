a9633428b6e24bf27bc87b1c09ac058b09bc88ac
kai <ks@juboka.de>
2015-12-18 12:03:32 +0100

    fix migrations

--- src/migrations/2015_12_18_133340_laikacms_user_update.php
+++ src/migrations/2015_12_18_133340_laikacms_user_update.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class LCMSUserUpdate extends Migration
+class LaikacmsAssetsFolderUpdate extends Migration
 {
     /**
      * Run the migrations.
